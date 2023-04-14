9b73ba331cf0331ec5b90787d42cc4f276d76314
neemkashu <sparjinkust@mail.ru>
2023-04-14 21:30:18 +0300

    feat: add deploy app url to cors

--- index.ts
+++ index.ts
@@ -1,30 +1,36 @@
-import express from 'express';
-import request from 'request';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import request from "request";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(cors({origin: 'http://localhost:5173'}))
 
-app.get('/', (req, res) => {
-    const url = 'https://api.flickr.com/services/rest';
-    console.log('req.url', url + req.url)
-    const apiUrl = new URL(url + req.url);
+const allowedOrigins = [
+  /^https?:\/\/deploy-preview-\d+--dancing-toffee-80fbd4\.netlify\.app$/,
+  "https://dancing-toffee-80fbd4.netlify.app/",
+  "http://localhost:5173",
+];
+app.use(cors({ origin: allowedOrigins }));
 
-    const apiParam = new URLSearchParams(apiUrl.searchParams);
-    apiParam.append('api_key', process.env.API_KEY ?? '');
-    apiUrl.search = apiParam.toString();
+app.get("/", (req, res) => {
+  const url = "https://api.flickr.com/services/rest";
+  console.log("req.url", url + req.url);
+  const apiUrl = new URL(url + req.url);
 
-    request.get(apiUrl.toString(), (err, response, body) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send(`Internal server error: ${err}`);
-        }
+  const apiParam = new URLSearchParams(apiUrl.searchParams);
+  apiParam.append("api_key", process.env.API_KEY ?? "");
+  apiUrl.search = apiParam.toString();
 
-        res.send(body);
-    });
+  request.get(apiUrl.toString(), (err, response, body) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(`Internal server error: ${err}`);
+    }
+
+    res.send(body);
+  });
 });
-app.listen(8080, '0.0.0.0', () => {
-    console.log(`Server started on port ${8080}`);
+app.listen(8080, "0.0.0.0", () => {
+  console.log(`Server started on port ${8080}`);
 });
